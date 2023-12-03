98d29a750548754bb03c68ec3f02ce34a4b68f43
Soni Bhandari <soni.bhandari@staqo.com>
2023-12-03 15:49:58 +0530

    header updated

---
+++ src/components/LandingPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../common/Header'
+
+const LandingPage = () => {
+  return (
+    <>
+    <Header />
+     <section className='hero-section'>
+        <h5>Welcome To </h5>
+        <h1>Kay Bakes</h1>
+     </section>
+        </>
+
+  )
+}
+
+export default LandingPage
